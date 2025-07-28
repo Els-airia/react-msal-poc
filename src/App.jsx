ff6743e7240936df6ac346d78a0d551ffd5f6103
Els-airia <airia@science.regn.net>
2025-07-28 13:58:08 +0530

    [RDPNEXUS-167]: MSAL POC Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+import React, {useState} from 'react';
+
+import { PageLayout } from './components/PageLayout';
+import { loginRequest } from './authConfig';
+import { callMsGraph } from './graph';
+import { ProfileData } from './components/ProfileData';
+
+import {AuthenticatedTemplate, UnauthenticatedTemplate, useMsal} from '@azure/msal-react';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+
+/**
+ * Renders information about the signed-in user or a button to retrieve data about the user
+ */
+
+const ProfileContent = () => {
+    const { instance, accounts } = useMsal();
+    const [graphData, setGraphData] = useState(null);
+
+    //fetch roles assigned to the user
+    const getRoles = () => {
+        instance.acquireTokenSilent({
+            scopes: ['User.Read'],
+            account: accounts[0],
+        })
+            .then(response => {
+                const idToken = response.idToken;
+                const decoded = parseJwt(idToken);
+                console.log("Assigned Roles: " + decoded.roles);
+            });
+    }
+    const parseJwt = (token) => {
+        const base64Payload = token.split('.')[1];
+        return JSON.parse(atob(base64Payload));
+    }
+
+    function RequestProfileData() {
+        // Silently acquires an access token which is then attached to a request for MS Graph data
+        instance
+            .acquireTokenSilent({
+                ...loginRequest,
+                account: accounts[0],
+            })
+            .then((response) => {
+                callMsGraph(response.accessToken).then((response) => setGraphData(response));
+            });
+    }
+
+    return (
+        <>
+            <h5 className="profileContent">Welcome {accounts[0].name}</h5>
+            {graphData ? (
+                <ProfileData graphData={graphData} />
+            ) : (
+                <>
+                <Button variant="secondary" onClick={RequestProfileData}>
+                    Request Profile
+                </Button>
+
+                <Button variant="secondary" onClick={getRoles}>
+                    Get Roles
+                </Button>
+                </>
+            )}
+        </>
+    );
+};
+
+/**
+ * If a user is authenticated the ProfileContent component above is rendered. Otherwise a message indicating a user is not authenticated is rendered.
+ */
+const MainContent = () => {
+    return (
+        <div className="App">
+            <AuthenticatedTemplate>
+                <ProfileContent />
+            </AuthenticatedTemplate>
+
+            <UnauthenticatedTemplate>
+                <h5 className="card-title">Please sign-in to see your profile information.</h5>
+            </UnauthenticatedTemplate>
+        </div>
+    );
+};
+
+export default function App() {
+    return (
+        <PageLayout>
+            <MainContent />
+        </PageLayout>
+    );
+}
